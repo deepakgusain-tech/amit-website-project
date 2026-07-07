79a620ce2cd4c5026c3697805c24d2ccf7504795
deepakgusain-tech <deepak.gusain@technosyslabs.com>
2026-07-07 14:29:46 +0530

    enquiry detail

--- app/(protected)/admin/enquiry/enquiry.tsx
+++ app/(protected)/admin/enquiry/enquiry.tsx
@@ -2,13 +2,15 @@
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { getEnquiryById } from '@/lib/actions/enquiry-action'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SingleEnquiry =  (props: any) => {
 
+    const [enquiry, setEnquiry] = useState<any>({})
+
     useEffect(() => {
       getEnquiryById(props.id).then((res) => {
-        console.log(res.data);
+        setEnquiry(res.data);
       })
     }, [props.id])
   
@@ -24,7 +26,12 @@ const SingleEnquiry =  (props: any) => {
                    
                 </SheetHeader>
                 <div className="grid flex-1 auto-rows-min gap-6 px-4">
-                    
+                    <h2>Full Name : {enquiry.fullName}</h2>
+                    <h2>Email : {enquiry.email}</h2>
+                    <h2>Phone : {enquiry.phoneNumber}</h2>
+                    <h2>Company Name : {enquiry.companyName}</h2>
+                    <h2>Message : {enquiry.message}</h2>   
+                    <h2>Subject : {enquiry.subject}</h2>    
                 </div>
                 <SheetFooter>
                     <SheetClose asChild>
